0f919a2ffda02c3a006cccc40fc07b8108d65214
Bjørn Dons <guardsmanpanda@gmail.com>
2022-07-03 22:03:30 +0200

    Update OidcToken.php
    oidc token update

--- src/Infrastructure/Oauth2/Dto/OidcToken.php
+++ src/Infrastructure/Oauth2/Dto/OidcToken.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use GuardsmanPanda\Larabear\Enum\BearSeverityEnum;
 use GuardsmanPanda\Larabear\Infrastructure\Integrity\Crud\BearIdempotencyCreator;
 use GuardsmanPanda\Larabear\Infrastructure\Security\Crud\BearSecurityIncidentCreator;
+use GuardsmanPanda\LarabearAuth\Infrastructure\Oauth2\Model\BearOauth2Client;
 use RuntimeException;
 use Throwable;
 
@@ -15,24 +16,26 @@ class OidcToken {
         public readonly string|null $name,
         public readonly string $email,
         public readonly string $issuedToClientId,
-        public readonly int    $notBefore,
+        public readonly int|null    $notBefore,
         public readonly int    $expiresAt,
     ) {}
 
-    public static function fromJwt(string $jwt, string $applicationId): self {
+    public static function fromJwt(string $jwt, BearOauth2Client $client): self {
         try {
-            $stdClass = json_decode(base64_decode(str_replace('_', '/', str_replace('-', '+', explode('.', $jwt)[1]))), false, 512, JSON_THROW_ON_ERROR);
-            if ($applicationId !== $stdClass->aud) {
+            $token = json_decode(base64_decode(str_replace('_', '/', str_replace('-', '+', explode('.', $jwt)[1]))), false, 512, JSON_THROW_ON_ERROR);
+            if ($client->oauth2_client_id !== $token->aud) {
                 BearSecurityIncidentCreator::create(
                     severity: BearSeverityEnum::CRITICAL,
                     namespace: "LarabearAuth",
                     headline: "Incorrect application id in JWT",
-                    description: "The application id in the JWT is not the same as the application id on the server. JWT: $stdClass->aud, Server: $applicationId",
+                    description: "The application id in the JWT is not the same as the application id on the server. JWT: $token->aud, Server: $applicationId",
                 );
                 throw new RuntimeException(message: "Incorrect Application ID.");
             }
-            $uniq = $stdClass->aud . ':' . ($stdClass->jti ?? $stdClass->uti);
-            BearIdempotencyCreator::create(idempotency_key: $uniq);
+            $uniq = $token->jti ?? $token->uti ?? null;
+            if ($uniq !== null) {
+                    BearIdempotencyCreator::create(idempotency_key: $token->aud . ':' . $uniq);
+            }
         } catch (Throwable $t) { //TODO Better error log.
             BearSecurityIncidentCreator::create(
                 severity: BearSeverityEnum::CRITICAL,
@@ -43,23 +46,34 @@ class OidcToken {
             throw new RuntimeException(message: "Token incorrectly formatted or already used.");
         }
 
+        if ((property_exists($token, property: 'email_verified') && $token->email_verified === false)) {
+            BearSecurityIncidentCreator::create(
+                severity: BearSeverityEnum::CRITICAL,
+                namespace: "LarabearAuth",
+                headline: "Invalid email address",
+                description: "The email address in the JWT is not verified.",
+            );
+            throw new RuntimeException(message: "Email address not verified.");
+        }
+
         $ts = Carbon::now()->timestamp;
-        if ($ts < $stdClass->nbf || $ts > $stdClass->exp) {
+        if ((property_exists($token, property: 'nbf') && $token->nbf) > $ts || $token->exp < $ts) {
             BearSecurityIncidentCreator::create(
                 severity: BearSeverityEnum::CRITICAL,
                 namespace: "LarabearAuth",
                 headline: "Invalid timestamp in JWT",
-                description: "The timestamp in the JWT is not valid. JWT: nbf: $stdClass->nbf, exp: $stdClass->exp, ts: $ts",
+                description: "The timestamp in the JWT is not valid. JWT: nbf: $token->nbf, exp: $token->exp, ts: $ts",
             );
             throw new RuntimeException(message: "Incorrect Timestamp.");
         }
+
         return new self(
-            userIdentifier: $stdClass->sub ?? $stdClass->oid,
-            name: $stdClass->name ?? null,
-            email: $stdClass->email,
-            issuedToClientId: $stdClass->aud,
-            notBefore: $stdClass->nbf,
-            expiresAt: $stdClass->exp
+            userIdentifier: $token->sub ?? $token->oid,
+            name: $token->name ?? $token->preferred_username ?? null,
+            email: $token->email,
+            issuedToClientId: $token->aud,
+            notBefore: $token->nbf ?? null,
+            expiresAt: $token->exp
         );
     }
 }
