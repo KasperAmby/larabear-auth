1139cc8b6122c207f5c7fddb4b519e58d377e17e
Bjørn Dons <guardsmanpanda@gmail.com>
2022-07-04 09:27:24 +0200

    fix jwt auth

--- src/Infrastructure/Oauth2/Service/BearOauth2UserService.php
+++ src/Infrastructure/Oauth2/Service/BearOauth2UserService.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Str;
 class BearOauth2UserService {
     public static function createOrUpdateFromCodeWithOidc(string $code, BearOauth2Client $client, string $redirectUri): BearOauth2User {
         $resp = BearOauth2ClientService::exchangeCode(code: $code, client: $client, redirect_uri: $redirectUri);
-        $token = OidcToken::fromJwt(jwt: $resp['id_token'], applicationId: $client->oauth2_client_id);
+        $token = OidcToken::fromJwt(jwt: $resp['id_token'], client: $client);
         $user = BearOauth2User::where(column: 'oauth2_client_id', operator: '=', value: $token->issuedToClientId)
             ->where(column: 'oauth2_user_identifier', operator: '=', value: $token->userIdentifier)
             ->first();
@@ -29,8 +29,9 @@ class BearOauth2UserService {
         $user->encrypted_user_refresh_token = $resp['refresh_token'] ?? null;
         $user->encrypted_user_access_token = $resp['access_token'];
 
-        $user->oauth2_scope = $resp['scope'];
-        foreach (explode(separator: ' ', string: $resp['scope']) as $scope) {
+        $user->oauth2_scope = is_array($resp['scope']) ? implode(' ', $resp['scope']) : $resp['scope'];
+        $scope_array = is_array($resp['scope']) ? $resp['scope'] : explode(' ', $resp['scope']);
+        foreach ($scope_array as $scope) {
             $user->oauth2_scope_jsonb->$scope = Carbon::now()->toDateString();
         }
 
