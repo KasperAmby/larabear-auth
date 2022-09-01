a95e7746bca9e59786fe004dbf330b2edcfd9cee
Kasper Amby <44236503+KasperAmby@users.noreply.github.com>
2022-09-01 21:27:02 +0200

    Set user and accesstoken in middleware
    When using the TokenUser middelware, the user id and token was not set in the global state.

--- src/Infrastructure/Http/Middleware/BearAccessTokenUserMiddleware.php
+++ src/Infrastructure/Http/Middleware/BearAccessTokenUserMiddleware.php
@@ -3,8 +3,10 @@
 namespace GuardsmanPanda\LarabearAuth\Infrastructure\Http\Middleware;
 
 use Closure;
+use GuardsmanPanda\Larabear\Enum\BearSeverityEnum;
 use GuardsmanPanda\Larabear\Infrastructure\App\Service\BearGlobalStateService;
 use GuardsmanPanda\Larabear\Infrastructure\Http\Service\Req;
+use GuardsmanPanda\Larabear\Infrastructure\Security\Crud\BearSecurityIncidentCreator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,15 +14,33 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class BearAccessTokenUserMiddleware {
     public function handle(Request $request, Closure $next) {
-        if ($request->bearerToken() === null) {
-            throw new AccessDeniedHttpException(message: 'The request must include a bearer token.');
-        }
-        $hashed_access_token = hash(algo: 'xxh128', data: $request->bearerToken());
-        $access = DB::selectOne("
+       if ($request->bearerToken() === null) {
+           throw new AccessDeniedHttpException(message: 'The request must include a bearer token.');
+       }
+       $hashed_access_token = hash(algo: 'xxh128', data: $request->bearerToken());
+       $access = DB::selectOne("
             SELECT at.id, at.user_id, at.expires_at, at.invalid_at
             FROM bear_access_token_user at
-            WHERE at.hashed_access_token = ? 
+            WHERE at.hashed_access_token = ?
+            AND at.expires_at > NOW()
+            AND at.invalid_at > NOW()
         ", [$hashed_access_token]);
+
+        // If access token is not valid, abort
+        if ($access === null || $access->id === null) {
+            $message = 'The supplied access token is not valid.. ip: ' . Req::ip() . ', country: ' . Req::ipCountry() . ', path: ' . Req::path() . ', hostname: '. Req::hostname() . ', hashed_token: ' . $hashed_access_token;
+            BearSecurityIncidentCreator::create(
+                severity: BearSeverityEnum::HIGH,
+                namespace: 'bear-token-auth',
+                headline: 'Invalid access token',
+                description: $message,
+                remediation: 'Check the system making the call, if it is not under your control then consider blacklisting the IP address.',
+            );
+            throw new AccessDeniedHttpException(message: $message);
+        }
+
+        BearGlobalStateService::setUserId($access->user_id);
+        BearGlobalStateService::setAccessTokenId($access->id);
         return $next($request);
     }
 
